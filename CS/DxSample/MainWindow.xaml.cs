810bbf1731a8a0a6dd37ce0ff5df980216829489
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DxSample/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using DevExpress.Xpf.Core;
+using System.Windows.Input;
+using System.Collections.Generic;
+
+namespace DxSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitDXTabControlItems(dXTabControl1);
+            InitAdditionItem(dXTabControl1);
+        }
+
+        private void InitDXTabControlItems(DXTabControl dXTabControl)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                dXTabControl.Items.Add("Page");
+            }
+        }
+
+        private void InitAdditionItem(DXTabControl dXTabControl)
+        {
+            DXTabItem item = new DXTabItem();
+            item.Header = "+";
+            item.PreviewMouseDown +=new MouseButtonEventHandler(AddItem_PreviewMouseDown);
+            item.ToolTip = "Open a new tab";
+            dXTabControl.Items.Add(item);
+        }
+
+        private void AddItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            dXTabControl1.Items.Insert(dXTabControl1.Items.Count-1,"New Page");
+            dXTabControl1.SelectedTabItem = dXTabControl1.GetTabItem(dXTabControl1.Items.Count - 2);
+            e.Handled = true;
+        }
+    }
+}
---
+++ VB/DxSample/MainWindow.xaml.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Windows
+Imports DevExpress.Xpf.Core
+Imports System.Windows.Input
+Imports System.Collections.Generic
+
+Namespace DxSample
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			InitDXTabControlItems(dXTabControl1)
+			InitAdditionItem(dXTabControl1)
+		End Sub
+
+		Private Sub InitDXTabControlItems(ByVal dXTabControl As DXTabControl)
+			For i As Integer = 0 To 14
+				dXTabControl.Items.Add("Page")
+			Next i
+		End Sub
+
+		Private Sub InitAdditionItem(ByVal dXTabControl As DXTabControl)
+			Dim item As New DXTabItem()
+			item.Header = "+"
+			AddHandler item.PreviewMouseDown, AddressOf AddItem_PreviewMouseDown
+			item.ToolTip = "Open a new tab"
+			dXTabControl.Items.Add(item)
+		End Sub
+
+		Private Sub AddItem_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			dXTabControl1.Items.Insert(dXTabControl1.Items.Count-1,"New Page")
+			dXTabControl1.SelectedTabItem = dXTabControl1.GetTabItem(dXTabControl1.Items.Count - 2)
+			e.Handled = True
+		End Sub
+	End Class
+End Namespace
