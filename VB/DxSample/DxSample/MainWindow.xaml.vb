013d1a70bb692908b0f70104c19c446a2bc52411
DevExpressExampleBot <fakeEmail>
2022-12-16 18:02:57 +0400

    Source auto update [skip ci]

--- VB/DxSample/DxSample/MainWindow.xaml.vb
+++ VB/DxSample/DxSample/MainWindow.xaml.vb
@@ -1,16 +1,12 @@
-﻿Imports System
-Imports System.Linq
-Imports System.Windows
 Imports DevExpress.Xpf.Core
-Imports System.Windows.Input
-Imports System.Collections.Generic
 
 Namespace DxSample
-	Partial Public Class MainWindow
-		Inherits DXWindow
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits DXWindow
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
